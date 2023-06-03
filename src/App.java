83fa2d9147e760c4bcbebcd00fe3e3560ad12a66
Jason Everton <jasonemsw10@gmail.com>
2023-06-03 09:32:52 -0300

    Commit1

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import boardgame.Position;
+
+public class App {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
